e32a23c53dbf46c35046326303b088298354c99e
Kiaan-18 <kapse.kiaan@gmail.com>
2025-04-06 11:00:33 +0200

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+a=18
+b=2
+c=1
+d=6
+value=a**d+b/a*b+c
+print("the value is",value)
+num1=int(input("enter a numerator:"))
+num2=int(input("enter a denominator:"))
+if num1%num2==0 :
+    print(num1,"is divisible by",num2,"is equal to",num1/num2)
+else:
+    print("it is not divisible by each other")
---
+++ activity2.py
@@ -0,0 +1,9 @@
+mean1=38
+wrong_num=36
+correct_num=56
+total_num=40
+sum=mean1*total_num
+print("the sum is",sum)
+sum2=sum-wrong_num+correct_num
+mean=sum2/total_num
+print("the new mean value for correct number is",mean)
